b87f9c80a176bd38ad7c39be022e9c18ce84cd50
andrew-zachary <andrew_zachary@outlook.com>
2023-04-22 00:00:00 +0000

    default-lang dir, ar-lang dir, handle [...slug] route

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -102,6 +102,146 @@ declare module 'astro:content' {
 
 	const entryMap: {
 		"posts": {
+"ar/post-1.md": {
+  id: "ar/post-1.md",
+  slug: "post-1",
+  body: string,
+  collection: "posts",
+  data: any
+} & { render(): Render[".md"] },
+"ar/post-10.md": {
+  id: "ar/post-10.md",
+  slug: "post-10",
+  body: string,
+  collection: "posts",
+  data: any
+} & { render(): Render[".md"] },
+"ar/post-11.md": {
+  id: "ar/post-11.md",
+  slug: "post-11",
+  body: string,
+  collection: "posts",
+  data: any
+} & { render(): Render[".md"] },
+"ar/post-12.md": {
+  id: "ar/post-12.md",
+  slug: "post-12",
+  body: string,
+  collection: "posts",
+  data: any
+} & { render(): Render[".md"] },
+"ar/post-13.md": {
+  id: "ar/post-13.md",
+  slug: "post-13",
+  body: string,
+  collection: "posts",
+  data: any
+} & { render(): Render[".md"] },
+"ar/post-14.md": {
+  id: "ar/post-14.md",
+  slug: "post-14",
+  body: string,
+  collection: "posts",
+  data: any
+} & { render(): Render[".md"] },
+"ar/post-15.md": {
+  id: "ar/post-15.md",
+  slug: "post-15",
+  body: string,
+  collection: "posts",
+  data: any
+} & { render(): Render[".md"] },
+"ar/post-16.md": {
+  id: "ar/post-16.md",
+  slug: "post-16",
+  body: string,
+  collection: "posts",
+  data: any
+} & { render(): Render[".md"] },
+"ar/post-17.md": {
+  id: "ar/post-17.md",
+  slug: "post-17",
+  body: string,
+  collection: "posts",
+  data: any
+} & { render(): Render[".md"] },
+"ar/post-18.md": {
+  id: "ar/post-18.md",
+  slug: "post-18",
+  body: string,
+  collection: "posts",
+  data: any
+} & { render(): Render[".md"] },
+"ar/post-19.md": {
+  id: "ar/post-19.md",
+  slug: "post-19",
+  body: string,
+  collection: "posts",
+  data: any
+} & { render(): Render[".md"] },
+"ar/post-2.md": {
+  id: "ar/post-2.md",
+  slug: "post-2",
+  body: string,
+  collection: "posts",
+  data: any
+} & { render(): Render[".md"] },
+"ar/post-20.md": {
+  id: "ar/post-20.md",
+  slug: "post-20",
+  body: string,
+  collection: "posts",
+  data: any
+} & { render(): Render[".md"] },
+"ar/post-3.md": {
+  id: "ar/post-3.md",
+  slug: "post-3",
+  body: string,
+  collection: "posts",
+  data: any
+} & { render(): Render[".md"] },
+"ar/post-4.md": {
+  id: "ar/post-4.md",
+  slug: "post-4",
+  body: string,
+  collection: "posts",
+  data: any
+} & { render(): Render[".md"] },
+"ar/post-5.md": {
+  id: "ar/post-5.md",
+  slug: "post-5",
+  body: string,
+  collection: "posts",
+  data: any
+} & { render(): Render[".md"] },
+"ar/post-6.md": {
+  id: "ar/post-6.md",
+  slug: "post-6",
+  body: string,
+  collection: "posts",
+  data: any
+} & { render(): Render[".md"] },
+"ar/post-7.md": {
+  id: "ar/post-7.md",
+  slug: "post-7",
+  body: string,
+  collection: "posts",
+  data: any
+} & { render(): Render[".md"] },
+"ar/post-8.md": {
+  id: "ar/post-8.md",
+  slug: "post-8",
+  body: string,
+  collection: "posts",
+  data: any
+} & { render(): Render[".md"] },
+"ar/post-9.md": {
+  id: "ar/post-9.md",
+  slug: "post-9",
+  body: string,
+  collection: "posts",
+  data: any
+} & { render(): Render[".md"] },
 "en/post-1.md": {
   id: "en/post-1.md",
   slug: "en/post-1",
