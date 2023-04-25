e327b184492abad920a713151a8b3c25cbe3aecf
andrew-zachary <andrew_zachary@outlook.com>
2023-04-25 00:00:00 +0000

    init scss/tailwind/fonts/ltr-rtl

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "./src/styles/_mixins.scss";'
+        },
+      },
+    }
+  }
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontSize: {
+				'cd-xs': '1.6rem',
+				'cd-sm': '2.2rem',
+				'cd-md': '3.2rem',
+				'cd-lg': '4rem',
+				'cd-xl': '5.8rem',
+				'cd-xxl': '6rem'
+			},
+			maxWidth: {
+				'cd-xs': '32rem',
+				'cd-sm': '48rem',
+				'cd-md': '76.8rem',
+				'cd-lg': '99.2rem'
+			},
+			boxShadow: {
+				'cd-base': '0 0.4rem 0.4rem black',
+			}
+		},
+	},
+	plugins: [],
+}
