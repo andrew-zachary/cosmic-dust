f3003231ccefe0c17bae9bdc0e520dafacb62520
andrew-zachary <andrew_zachary@outlook.com>
2023-04-29 00:00:00 +0000

    filter categories by search-input

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,28 @@ import alpine from 'alpinejs';
 import collapse from '@alpinejs/collapse';
 
 const init = () => {
+
+    // alpine contexts
     alpine.data('navmenu', () => ({
         'open': false
     }));
+    alpine.data('searchingCategories', () => ({
+        
+        filterCategories(e) {
+
+            const catsElements = Array.from(this.$refs.cats.querySelectorAll('li'));
+
+            for (const ele of catsElements) {
+                if (ele.textContent.includes(e.target.value)) ele.classList.remove('exclude');
+                else ele.classList.add('exclude');
+            }
+
+        }
+
+    }));
+
     alpine.plugin(collapse);
+
     alpine.start();
 };
 
